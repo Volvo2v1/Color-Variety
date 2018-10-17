840c9839569f61979a2985d51cc7bb331b051989
Volvo2v1 <volvo2v1@gmail.com>
2018-10-17 09:04:00 +0200

    Complete Usage Showcase

---
+++ example_usage.c
@@ -0,0 +1,94 @@
+#include "clr_vrt.h"
+
+int main(int argc, char const *argv[])
+{
+    printf_red("Red QWERTY123\n");
+	printf_lightred("Light-Red QWERTY123\n");
+	printf_darkred("Dark-Red QWERTY123\n");
+	printf_green("Green QWERTY123\n");
+	printf_lightgreen("Light-Green QWERTY123\n");
+	printf_darkgreen("Dark-Green QWERTY123\n");
+	printf_lime("Lime QWERTY123\n");
+	printf_yellow("Yellow QWERTY123\n");
+	printf_lightyellow("Light-Yellow QWERTY123\n");
+	printf_darkyellow("Dark-Yellow QWERTY123\n");
+	printf_orange("Orange QWERTY123\n");
+	printf_lightorange("Light-Orange QWERTY123\n");
+	printf_darkorange("Dark-Orange QWERTY123\n");
+	printf_blue("Blue QWERTY123\n");
+	printf_lightblue("Light-Blue QWERTY123\n");
+	printf_navy("Navy QWERTY123\n");
+	printf_darkblue("Dark-Blue QWERTY123\n");
+	printf_cyan("Cyan QWERTY123\n");
+	printf_neoncyan("Neon-Cyan QWERTY123\n");
+	printf_purple("Purple QWERTY123\n");
+	printf_lightpurple("Light-Purple QWERTY123\n");
+	printf_darkpurple("Dark-Purple QWERTY123\n");
+	printf_pink("Pink QWERTY123\n");
+	printf_lightpink("Light-Pink QWERTY123\n");
+	printf_neonpink("Neon-Pink QWERTY123\n");
+	printf_darkpink("Dark-Pink QWERTY123\n");
+	printf_fuchsia("Fuchsia QWERTY123\n");
+	printf_magenta("Magenta QWERTY123\n");
+	printf_black("Black QWERTY123\n");
+	printf_grey("Grey QWERTY123\n");
+	printf_white("White QWERTY123\n");
+	
+	printf_bold_red("Bold Red QWERTY123\n");
+	printf_bold_lightred("Bold Light-Red QWERTY123\n");
+	printf_bold_darkred("Bold Dark-Red QWERTY123\n");
+	printf_bold_green("Bold Green QWERTY123\n");
+	printf_bold_lightgreen("Bold Light-Green QWERTY123\n");
+	printf_bold_darkgreen("Bold Dark-Green QWERTY123\n");
+	printf_bold_lime("Bold Lime QWERTY123\n");
+	printf_bold_yellow("Bold Yellow QWERTY123\n");
+	printf_bold_lightyellow("Bold Light-Yellow QWERTY123\n");
+	printf_bold_darkyellow("Bold Dark-Yellow QWERTY123\n");
+	printf_bold_orange("Bold Orange QWERTY123\n");
+	printf_bold_lightorange("Bold Light-Orange QWERTY123\n");
+	printf_bold_darkorange("Bold Dark-Orange QWERTY123\n");
+	printf_bold_blue("Bold Blue QWERTY123\n");
+	printf_bold_lightblue("Bold Light-Blue QWERTY123\n");
+	printf_bold_navy("Bold Navy QWERTY123\n");
+	printf_bold_darkblue("Bold Dark-Blue QWERTY123\n");
+	printf_bold_cyan("Bold Cyan QWERTY123\n");
+	printf_bold_neoncyan("Bold Neon-Cyan QWERTY123\n");
+	printf_bold_purple("Bold Purple QWERTY123\n");
+	printf_bold_lightpurple("Bold Light-Purple QWERTY123\n");
+	printf_bold_darkpurple("Bold Dark-Purple QWERTY123\n");
+	printf_bold_pink("Bold Pink QWERTY123\n");
+	printf_bold_lightpink("Bold Light-Pink QWERTY123\n");
+	printf_bold_neonpink("Bold Neon-Pink QWERTY123\n");
+	printf_bold_darkpink("Bold Dark-Pink QWERTY123\n");
+	printf_bold_fuchsia("Bold Fuchsia QWERTY123\n");
+	printf_bold_magenta("Bold Magenta QWERTY123\n");
+	printf_bold_black("Bold Black QWERTY123\n");
+	printf_bold_grey("Bold Grey QWERTY123\n");
+	printf_bold_white("Bold White QWERTY123\n");
+
+	printf_bold_pink("\nS");
+	printf_bold_yellow("P");
+	printf_bold_blue("E");
+	printf_bold_red("C");
+	printf_bold_neoncyan("I");
+	printf_bold_candyapple("A");
+	printf_bold_neonpink("L\n\n");
+
+	printf_syrup("Syrup QWERTY123\n"); 
+	printf_amaranth("Amaranth QWERTY123\n");
+	printf_olive("Olive QWERTY123\n");
+	printf_candyapple("Candy Apple QWERTY123\n");
+	printf_emerald("Emerald QWERTY123\n");
+	printf_forestgreen("Forest Green QWERTY123\n");
+	printf_caramel("Caramel QWERTY123\n");
+
+	printf_bold_syrup("Bold Syrup QWERTY123\n"); 
+	printf_bold_amaranth("Bold Amaranth QWERTY123\n");
+	printf_bold_olive("Bold Olive QWERTY123\n");
+	printf_bold_candyapple("Bold Candy Apple QWERTY123\n");
+	printf_bold_emerald("Bold Emerald QWERTY123\n");
+	printf_bold_forestgreen("Bold Forest Green QWERTY123\n");
+	printf_bold_caramel("Bold Caramel QWERTY123\n");
+
+	return 0;
+}
