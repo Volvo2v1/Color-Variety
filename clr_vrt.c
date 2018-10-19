b75a14c2528dd310e2f3b7fa770410cb70ca3e4a
Volvo2v1 <volvo2v1@gmail.com>
2018-10-19 04:59:04 +0200

    FIXED COPY/PASTA TYPOS

--- clr_vrt.c
+++ clr_vrt.c
@@ -317,134 +317,103 @@ void printf_bold_white(char *__std_out) {
 
 void clr_red() {
     printf("\033[0;31m");
-    resetcolor();
 }            // \033[0;31m
 void clr_lightred() {
     printf("\033[38;5;203m");
-    resetcolor();
 }      // \033[38;5;203m
 void clr_darkred() {
     printf("\033[38;5;1m");
-    resetcolor();
 }       // \033[38;5;1m
 
 void clr_green() {
     printf("\033[0;32m");
-    resetcolor();
 }         // \033[0;32m
 void clr_lightgreen() {
     printf("\033[38;5;10m");
-    resetcolor();
 }    // \033[38;5;10m
 void clr_darkgreen() {
     printf("\033[38;5;34m");
-    resetcolor();
 }     // \033[38;5;34m
 void clr_lime() {
     printf("\033[38;5;118m");
-    resetcolor();
 }          // \033[38;5;118m
 
 void clr_yellow() {
     printf("\033[38;5;226m");
-    resetcolor();
 }        // \033[38;5;226m
 void clr_lightyellow() {
     printf("\033[38;5;227m");
-    resetcolor();
 }   // \033[38;5;227m
 void clr_darkyellow() {
     printf("\033[38;5;220m");
-    resetcolor();
 }    // \033[38;5;220m
 
 void clr_orange() {
     printf("\033[38;5;208m");
-    resetcolor();
 }        // \033[38;5;208m
 void clr_lightorange() {
     printf("\033[38;5;214m");
-    resetcolor();
 }   // \033[38;5;214m
 void clr_darkorange() {
     printf("\033[38;5;202m");
-    resetcolor();
 }    // \033[38;5;202m
 
 void clr_blue() {
     printf("\033[0;34m");
-    resetcolor();
 }          // \033[0;34m
 void clr_lightblue() {
     printf("\033[38;5;33m");
-    resetcolor();
 }     // \033[38;5;33m
 void clr_navy() {
     printf("\033[38;5;21m");
-    resetcolor();
 }          // \033[38;5;21m 
 void clr_darkblue() {
     printf("\033[38;5;19m");
-    resetcolor();
 }      // \033[38;5;19m
 void clr_cyan() {
     printf("\033[38;5;14m");
-    resetcolor();
 }          // \033[38;5;14m
 void clr_neoncyan() {
     printf("\033[38;5;51m");
-    resetcolor();
 }      // \033[38;5;51m
 
 void clr_purple() {
     printf("\033[0;35m");
-    resetcolor();
 }        // \033[0;35m
 void clr_lightpurple() {
     printf("\033[38;5;93m");
-    resetcolor();
 }   // \033[38;5;93m
 void clr_darkpurple() {
     printf("\033[38;5;91m");
-    resetcolor();
 }    // \033[38;5;91m
 
 void clr_pink() {
     printf("\033[38;5;205m");
-    resetcolor();
 }          // \033[38;5;205m
 void clr_lightpink() {
     printf("\033[38;5;218m");
-    resetcolor();
 }     // \033[38;5;218m
 void clr_neonpink() {
     printf("\033[38;5;198m");
-    resetcolor();
 }      // \033[38;5;198m
 void clr_darkpink() {
     printf("\033[38;5;197m");
-    resetcolor();
 }      // \033[38;5;197m
 void clr_fuchsia() {
     printf("\033[38;5;164m");
-    resetcolor();
 }       // \033[38;5;164m
 void clr_magenta() {
     printf("\033[38;5;200m");
-    resetcolor();
 }       // \033[38;5;200m
 
 void clr_black() {
     printf("\033[38;5;0m");
-    resetcolor();
 } // \033[38;5;0m
 void clr_grey() {
     printf("\033[38;5;7m");
-    resetcolor();
 }  // \033[38;5;7m
 void clr_white() {
     printf("\033[0;37m");
-    resetcolor();
 } // \033[0;37m
 
 
@@ -452,134 +421,103 @@ void clr_white() {
 
 void bold_clr_red() {
     printf("\033[1;31m");
-    resetcolor();
 }           // \033[1;31m
 void bold_clr_lightred() {
     printf("\033[1;38;5;203m");
-    resetcolor();
 }      // \033[1;38;5;203m
 void bold_clr_darkred() {
     printf("\033[1;38;5;1m");
-    resetcolor();
 }       // \033[1;38;5;1m
 
 void bold_clr_green() {
     printf("\033[1;32m");
-    resetcolor();
 }         // \033[1;32m
 void bold_clr_lightgreen() {
     printf("\033[1;38;5;10m");
-    resetcolor();
 }    // \033[1;38;5;10m
 void bold_clr_darkgreen() {
     printf("\033[1;38;5;34m");
-    resetcolor();
 }     // \033[1;38;5;34m
 void bold_clr_lime() {
     printf("\033[1;38;5;118m");
-    resetcolor();
 }          // \033[1;38;5;118m
 
 void bold_clr_yellow() {
     printf("\033[1;38;5;226m");
-    resetcolor();
 }        // \033[1;38;5;226m
 void bold_clr_lightyellow() {
     printf("\033[1;38;5;227m");
-    resetcolor();
 }   // \033[1;38;5;227m
 void bold_clr_darkyellow() {
     printf("\033[1;38;5;220m");
-    resetcolor();
 }    // \033[1;38;5;220m
 
 void bold_clr_orange() {
     printf("\033[1;38;5;208m");
-    resetcolor();
 }        // \033[1;38;5;208m
 void bold_clr_lightorange() {
     printf("\033[1;38;5;214m");
-    resetcolor();
 }   // \033[1;38;5;214m
 void bold_clr_darkorange() {
     printf("\033[1;38;5;202m");
-    resetcolor();
 }    // \033[1;38;5;202m
 
 void bold_clr_blue() {
     printf("\033[1;34m");
-    resetcolor();
 }         // \033[1;34m
 void bold_clr_lightblue() {
     printf("\033[1;38;5;33m");
-    resetcolor();
 }    // \033[1;38;5;33m
 void bold_clr_navy() {
     printf("\033[1;38;5;21m");
-    resetcolor();
 }         // \033[1;38;5;21m 
 void bold_clr_darkblue() {
     printf("\033[1;38;5;19m");
-    resetcolor();
 }     // \033[1;38;5;19m
 void bold_clr_cyan() {
     printf("\033[1;38;5;14m");
-    resetcolor();
 }         // \033[1;38;5;14m
 void bold_clr_neoncyan() {
     printf("\033[1;38;5;51m");
-    resetcolor();
 }     // \033[1;38;5;51m
 
 void bold_clr_purple() {
     printf("\033[1;35m");
-    resetcolor();
 }        // \033[1;35m
 void bold_clr_lightpurple() {
     printf("\033[1;38;5;93m");
-    resetcolor();
 }   // \033[1;38;5;93m
 void bold_clr_darkpurple() {
     printf("\033[1;38;5;91m");
-    resetcolor();
 }    // \033[1;38;5;91m
 
 void bold_clr_pink() {
     printf("\033[1;38;5;205m");
-    resetcolor();
 }         // \033[1;38;5;205m
 void bold_clr_lightpink() {
     printf("\033[1;38;5;218m");
-    resetcolor();
 }    // \033[1;38;5;218m
 void bold_clr_neonpink() {
     printf("\033[1;38;5;198m");
-    resetcolor();
 }     // \033[1;38;5;198m
 void bold_clr_darkpink() {
     printf("\033[1;38;5;197m");
-    resetcolor();
 }     // \033[1;38;5;197m
 void bold_clr_fuchsia() {
     printf("\033[1;38;5;164m");
-    resetcolor();
 }      // \033[1;38;5;164m
 void bold_clr_magenta() {
     printf("\033[1;38;5;200m");
-    resetcolor();
 }      // \033[1;38;5;200m
 
 void bold_clr_black() {
     printf("\033[1;38;5;0m");
-    resetcolor();
 } // \033[1;38;5;0m
 void bold_clr_grey() {
     printf("\033[1;38;5;7m");
-    resetcolor();
 }  // \033[1;38;5;7m
 void bold_clr_white() {
     printf("\033[1;37m");
-    resetcolor();
 } // \033[1;37m
 
 
